d4fb33853c2f90360e28476177d919ea10fb0436
JuneJH <15310487388@163.com>
2022-03-20 20:14:21 +0800

    feat(*): java循环练习

---
+++ src/Loop.java
@@ -0,0 +1,30 @@
+public  class  Loop{
+    public static void main(String[] args){
+        comp();
+        chickRabbit();
+    }
+    // 加工零件370
+    // a 加工零件 +10
+    // b 加工零件 +20
+    // c 加工零件 *2
+    // d 加工零件 /2
+    // 则a == b == c == d;
+    public static void comp(){
+        int a = 0, b = 0, c = 0, d = 0;
+        for(int i = 1; i < 370; i ++){
+            if(i - 10 +  i + 20 + i / 2 + i * 2 == 370 ){
+                System.out.println("四个工人分别加工"+  (i - 10) +","+  (i + 20) +","+  (i / 2 ) +","+  (i * 2));
+            }
+        }
+    }
+    // 鸡兔同笼
+    // 共50
+    // 160只脚
+    public static void chickRabbit(){
+        for(int i = 0; i < 50; i ++){
+            if(i * 2 + (50 - i) * 4 == 160){
+                System.out.println("鸡有"+i+"只"+"兔子有"+ (50 - i)+"只");
+            }
+        }
+    }
+}
