3a3d7d9e793ef5a3d6453d630eef5fd89927b0a1
JuneJH <15310487388@163.com>
2022-04-18 22:50:04 +0800

    feat(*): 封装一个Arraybox

--- src/ArrayBox.java
+++ src/ArrayBox.java
@@ -1,43 +1,104 @@
-public class ArrayBox {
-    public int[] array;
-
-    public ArrayBox(){}
-    public ArrayBox(int[] array){
-        this.array = array;
+public class ArrayBox<E> {
+    private E[] array;
+    private static final int DEFAULT_SIZE = 15;
+    private int size = 0;
+    public ArrayBox(){
+        this.array =(E[]) new Object[DEFAULT_SIZE];
     }
-    public ArrayBox(String[] array){
-//        this.a = array;
+    public ArrayBox(int size){
+        this.array =(E[]) new Object[size];
     }
 
-    public int[] push(int...array){
-        int[] arr = new int[array.length + this.array.length];
-        int index = 0;
-        for(int val : this.array){
-            arr[index ++] = val;
+    /**
+     * 判断是否越界
+     * @param index
+     * @return
+     */
+    private boolean isIndexOut(int index){
+        if(this.size > index && index >= 0){
+            return  true;
         }
-        for(int val : array){
-            arr[index ++] = val;
-        }
-        return  this.array = arr;
-    }
-    public int getSize(){
-        return  this.array.length;
+        return  false;
     }
 
-
-    public static void main(String[] arg){
-        int[] array = {1,2,3,4,5};
-        ArrayBox arraybox = new ArrayBox(array);
-
-        arraybox.push(6,7,8,9);
-
-        System.out.println("当前数组");
-        for(int val : arraybox.array){
-            System.out.println(val);
+    private void expansion(int size){
+        if(this.array.length < size + this.size){
+            Object[] arr = new Object[this.size + size + size / 2];
+            int let = arr.length;
+            System.out.println("lennnn"+let);
+            for(int i = 0; i < this.array.length; i ++){
+                arr[i] = this.array[i];
+            }
+            this.array = (E[]) arr;
         }
-        System.out.println("当前数组的长度"+ arraybox.getSize());
+    }
 
+    /**
+     * push 一组数据
+     * @param array
+     * @return
+     */
+    public E[] push(E...array){
+        this.expansion(array.length);
+        for(E val : array){
+            this.array[this.size ++] = val;
+        }
+        return  this.array;
+    }
 
+    /**
+     * 获取size
+     * @return
+     */
+    public int size(){
+        return this.size;
+    }
 
+    /**
+     * 根据索引得到对应位置的值
+     * @param index
+     * @return
+     */
+    public E get(int index){
+        if(this.isIndexOut(index)){
+            return (E) this.array[index];
+        }else{
+            throw new BoxIndexOutException("越界");
+        }
+    }
+
+    /**
+     * 根据索引移除元素
+     * @param index
+     * @return
+     */
+    public E remove(int index){
+        if(this.isIndexOut(index)){
+            for(int i = index; i < this.size; i ++){
+                this.array[i] = this.array[i + 1];
+            }
+            this.array[--this.size] = null;
+        }else {
+            throw new BoxIndexOutException("越界");
+        }
+        return  this.get(index);
+    }
+
+    /**
+     * 打印数组元素
+     */
+    public void print(){
+        String str = "";
+        for(int i = 0; i < this.size; i ++){
+            str += this.array[i] + ",";
+        }
+        System.out.println(str.substring(0,str.length() - 1));
+    }
+
+    public E modifyByIndex(int index,E val){
+        if(this.isIndexOut(index)){
+            this.array[index] = val;
+        }
+        return  this.array[index];
     }
 }
