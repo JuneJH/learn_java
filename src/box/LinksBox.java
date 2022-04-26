1b7f6179fe8d4ecc0a359a418a55399190132f4a
JuneJH <15310487388@163.com>
2022-04-26 22:35:05 +0800

    feat(*): 重构linkBox 内部类Node

--- src/box/LinksBox.java
+++ src/box/LinksBox.java
@@ -2,10 +2,46 @@ package box;
 
 public class LinksBox<T> implements Box<T>{
 
+    private class Node<T>{
+
+        private Node pre;
+        private Node next;
+        private T val;
+
+        public Node(Node pre,T val,Node next){
+            this.next = next;
+            this.pre = pre;
+            this.val = val;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public Node getPre() {
+            return pre;
+        }
+
+        public T getVal() {
+            return val;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public void setPre(Node pre) {
+            this.pre = pre;
+        }
+
+        public void setVal(T val) {
+            this.val = val;
+        }
+    }
+
     private Node<T> first;
     private Node<T> last;
     private int size;
-
 
     private void linkNode(T element){
         Node lastNode = last;
