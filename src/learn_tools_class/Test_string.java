fa70ce17531e320fbd665c2e37f8d70d25fef298
JuneJH <15310487388@163.com>
2022-05-12 00:00:04 +0800

    feat(*): String

---
+++ src/learn_tools_class/Test_string.java
@@ -0,0 +1,12 @@
+package learn_tools_class;
+
+public class Test_string {
+
+    public static void main(String[] args) {
+
+
+        String str1 = "string";
+        String str2 = "string";
+        String str3 = new String("string");
+    }
+}
