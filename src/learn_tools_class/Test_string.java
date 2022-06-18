9765eb9751bab3dd3e9f17cb264f57ca70aacf06
JuneJH <15310487388@163.com>
2022-06-18 21:01:53 +0800

    feat(*): String

--- src/learn_tools_class/Test_string.java
+++ src/learn_tools_class/Test_string.java
@@ -1,13 +1,48 @@
 package learn_tools_class;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Test_string {
 
     public static void main(String[] args) {
 
+        // StringBuffer: 同步的，安全高，效率低
+        // StringBuilder
 
-        String str1 = "string";
+        // substring() replace(); 使用不相同
+
+        // String
+
+
+
+        String str1 = "iii";
         String str2 = "string";
         String str3 = new String("string");
+
+        str1.compareTo(str2);
+
+
+        System.out.println(str1 == str2);
+        System.out.println(str1 == str3);
+        System.out.println(str3);
+
+
+        boolean isFlag = str1.matches("[i]{2,3}");
+
+        System.out.println(isFlag);
+
+        Pattern pattern = Pattern.compile("[gp][eo][ts][t]?");
+
+        String methods = "getsdlfjpostsklfj;lgetasdfjl;postsdl;kfjl;get";
+
+        Matcher matcher = pattern.matcher(methods);
+
+        while (matcher.find()){
+            System.out.println(matcher.group());
+        }
+
+
+
 
 
 
