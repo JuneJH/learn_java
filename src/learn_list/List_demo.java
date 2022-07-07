ca239435fe36acec5a6bf9523a3068efa486ea7f
JuneJH <15310487388@163.com>
2022-07-07 08:02:45 +0800

    feat(*): ArrayList

--- src/learn_list/List_demo.java
+++ src/learn_list/List_demo.java
@@ -7,17 +7,22 @@ public class List_demo {
 
     public static void main(String[] args) {
 
+
+        // collection  存储的都是value值
+        // collection =>  List Set
+        // map          存储的都是key-value
+
         ArrayList<String> al1 = new ArrayList<>();
 
         ArrayList<String> al = new ArrayList<>();
         al.add("age");
         al.add("name");     //  新增
-        al.addAll(al1);      // 并集
+        al.addAll(al1);     // 并集
         al.remove(0); // 删除
         al.removeAll(al1);   // 差集
         al.get(0);          // 获取
         al.size();          // 得到长度
-        al.clear();         // 清楚
+        al.clear();         // 清除
         al.contains(1);     // 是否存在
         al.indexOf(1);      // 找到该元素的位置
         al.lastIndexOf(2);//
@@ -31,9 +36,6 @@ public class List_demo {
         al.toArray(arr);  // 通过参数确定类型
         al.trimToSize(); // 变成有效长度
 
-        // collection  存储的都是value值
-        // collection =>  List Set
-        // map          存储的都是key-value
 
 
     }
